fbf91970ff692f2785429792e00d8703f5c79c3c
novaya-zemlya <155776706+novaya-zemlya@users.noreply.github.com>
2025-06-18 06:43:00 -0400

    hit updates for all buttons in t

--- src/pages/Tutorials (WIP).uodu2.js
+++ src/pages/Tutorials (WIP).uodu2.js
@@ -19,4 +19,63 @@ $w.onReady(() => {
    
     }
   });
+
+
+
+  $w("#elecGo").onClick(async () => {
+    const itemId = "64e0a010-a87e-4b59-a0ff-c60d713e6181"; // id of the elec item, if cms is changed this must also change, dont forget future me (or anyone else)
+    try {
+      
+      const item = await wixData.get("hits", itemId); // get the item from the collection
+
+      
+      item.Number = (item.Number || 0) + 1; // hits ++
+
+      
+      const updatedItem = await wixData.update("hits", item); // call the update 
+
+      console.log("Updated number:", updatedItem.number);
+    } catch (err) {
+      console.error("Failed to update item:", err.message);
+   
+    }
+  });
+
+  $w("#softGo").onClick(async () => {
+    const itemId = "1fd7dae1-a4e0-4b18-a849-86b385c90711"; // id of the admin item, if cms is changed this must also change, dont forget future me (or anyone else)
+    try {
+      
+      const item = await wixData.get("hits", itemId); // get the item from the collection
+
+      
+      item.Number = (item.Number || 0) + 1; // hits ++
+
+      
+      const updatedItem = await wixData.update("hits", item); // call the update 
+
+      console.log("Updated number:", updatedItem.number);
+    } catch (err) {
+      console.error("Failed to update item:", err.message);
+   
+    }
+  });
+
+  $w("#mechGo").onClick(async () => {
+    const itemId = "749e1974-eae9-40e8-b379-48b1eee2b381"; // id of the admin item, if cms is changed this must also change, dont forget future me (or anyone else)
+    try {
+      
+      const item = await wixData.get("hits", itemId); // get the item from the collection
+
+      
+      item.Number = (item.Number || 0) + 1; // hits ++
+
+      
+      const updatedItem = await wixData.update("hits", item); // call the update 
+
+      console.log("Updated number:", updatedItem.number);
+    } catch (err) {
+      console.error("Failed to update item:", err.message);
+   
+    }
+  });
 });
